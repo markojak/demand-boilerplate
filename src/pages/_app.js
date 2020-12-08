3614ec71d286c21721db02f4a0411089fde48229
adamjuhasz <adamjuhasz@me.com>
2020-12-08 16:37:03 +0100

    - changing name and author in package.json

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -2,7 +2,8 @@ import React, { Fragment } from 'react';
 import { Modal } from '@redq/reuse-modal';
 import '@redq/reuse-modal/es/index.css';
 import 'rc-tabs/assets/index.css';
-import '../common/src/assets/css/flaticon.css';
+import 'src/common/src/assets/css/flaticon.css';
+import 'src/common/src/assets/css/icon-example-page.css';
 
 function MyApp({ Component, pageProps }) {
   return (
