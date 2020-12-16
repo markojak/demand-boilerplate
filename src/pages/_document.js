2cc1751665697ae4ef3f720d59304fe00d837095
adamjuhasz <adamjuhasz@me.com>
2020-12-16 17:08:45 +0100

    - changing GTM

--- src/pages/_document.js
+++ src/pages/_document.js
@@ -37,15 +37,8 @@ export default class CustomDocument extends Document {
 
           {!!process.env.gtmCode && (
             <script
-              dangerouslySetInnerHTML={{
-                __html: `
-  (function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
-new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
-j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
-'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
-})(window,document,'script','dataLayer','${process.env.gtmCode}');
-`
-              }}
+              async
+              src={`https://www.googletagmanager.com/gtag/js?id=${process.env.gtmCode}`}
             />
           )}
 
@@ -71,16 +64,6 @@ j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
           )}
         </Head>
         <body>
-          {!!process.env.gtmCode && (
-            <noscript
-              dangerouslySetInnerHTML={{
-                __html: `
-            <iframe src="https://www.googletagmanager.com/ns.html?id=${process.env.gtmCode}"
-height="0" width="0" style="display:none;visibility:hidden"></iframe>
-            `
-              }}
-            />
-          )}
           <Main />
           <NextScript />
         </body>
