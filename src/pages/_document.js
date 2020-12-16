2b71cc6baa377ae6d6f8d5242d3ede6602847e57
adamjuhasz <adamjuhasz@me.com>
2020-12-16 19:07:25 +0100

    using the same google ID for both tag

--- src/pages/_document.js
+++ src/pages/_document.js
@@ -34,26 +34,24 @@ export default class CustomDocument extends Document {
       <Html lang="en">
         <Head>
           <link rel="shortcut icon" type="image/x-icon" href={FavIcon} />
-
-          {!!process.env.gtmCode && (
-            <script
-              async
-              src={`https://www.googletagmanager.com/gtag/js?id=${process.env.gtmCode}`}
-            />
-          )}
-
           {!!process.env.gaCode && (
-            <script
-              dangerouslySetInnerHTML={{
-                __html: `
+            <>
+              <script
+                async
+                src={`https://www.googletagmanager.com/gtag/js?id=${process.env.gaCode}`}
+              />
+              <script
+                dangerouslySetInnerHTML={{
+                  __html: `
   window.dataLayer = window.dataLayer || [];
   function gtag(){dataLayer.push(arguments);}
   gtag('js', new Date());
 
   gtag('config', '${process.env.gaCode}');
 `
-              }}
-            />
+                }}
+              />
+            </>
           )}
 
           {!!process.env.gSiteVer && (
